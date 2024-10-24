0df58f3ad05d4ae75a9dca1519dcddae9c899704
t-ishitsuka <takuya.ishitsuka@gmail.com>
2024-10-24 09:55:48 +0900

    update: socket addr

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use std::net::{Ipv4Addr, SocketAddr};
+
 use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello world" }));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8000").await.unwrap();
-    axum::serve(listener, app).await.unwrap()
+    let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8000);
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+
+    println!("Server listening on the {}", addr);
+
+    axum::serve(listener, app).await.unwrap();
 }
