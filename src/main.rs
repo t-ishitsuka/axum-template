9bdfb2568d3c70d772b039f0cc911828740f8419
t-ishitsuka <takuya.ishitsuka@gmail.com>
2024-10-24 10:01:27 +0900

    update: health check, add: anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
-use axum::{routing::get, Router};
+use anyhow::Result;
+use axum::{http::StatusCode, routing::get, Router};
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello world" }));
+async fn main() -> Result<()> {
+    let app = Router::new()
+        .route("/", get(|| async { "Hello world" }))
+        .route("/health-check", get(health_check));
 
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8000);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
 
     println!("Server listening on the {}", addr);
 
-    axum::serve(listener, app).await.unwrap();
+    Ok(axum::serve(listener, app).await?)
+}
+
+pub async fn health_check() -> StatusCode {
+    StatusCode::OK
 }
