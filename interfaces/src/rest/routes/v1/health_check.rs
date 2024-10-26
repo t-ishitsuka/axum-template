c5b126165e5303402af6a889df5a281872e99235
t-ishitsuka <takuya.ishitsuka@gmail.com>
2024-10-26 14:21:38 +0900

    feat: health check controller

--- interfaces/src/rest/routes/v1/health_check.rs
+++ interfaces/src/rest/routes/v1/health_check.rs
@@ -1,11 +1,9 @@
-use axum::{http::StatusCode, routing::get, Router};
+use axum::{routing::get, Router};
+
+use crate::rest::controllers::v1::health_check_controller::health_check;
 
 pub fn build_health_check_router() -> Router {
     let router = Router::new().route("/", get(health_check));
 
     Router::new().nest("/health-check", router)
-}
-
-pub async fn health_check() -> StatusCode {
-    StatusCode::OK
 }
