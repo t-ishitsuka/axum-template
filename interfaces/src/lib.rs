ef6948452b03d7cf80e33649e46f840abf0d1b7f
t-ishitsuka <takuya.ishitsuka@gmail.com>
2024-10-26 14:24:25 +0900

    build router

--- interfaces/src/lib.rs
+++ interfaces/src/lib.rs
@@ -1,1 +1,8 @@
+use axum::Router;
+use rest::routes::build_rest_router;
+
 pub mod rest;
+
+pub fn build_router() -> Router {
+    Router::new().merge(build_rest_router())
+}
--- src/bin/app.rs
+++ src/bin/app.rs
@@ -1,9 +1,8 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
 use anyhow::{Error, Result};
-use axum::Router;
 
-use interfaces::rest::routes::build_rest_router;
+use interfaces::build_router;
 
 /**
  * Main
@@ -17,7 +16,7 @@ async fn main() -> Result<()> {
  * Start application server
  */
 async fn bootstrap() -> Result<()> {
-    let app = Router::new().merge(build_rest_router());
+    let app = build_router();
 
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8000);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
